09681455716a85d1b5f959d5e5694a33f1142f37
Barryonggh <Barryong.ogh@gmail.com>
2024-05-21 12:03:12 +0800

    react version of rarefishproject

---
+++ fishinvestv2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header';
+import MainContent from './MainContent,';
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ fishinvestv2/src/MainContent,.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContent = () => (
+  <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+    <h1>Rare fish: The future of finance</h1>
+    <img src="https://i.imgur.com/7Ja7HbS.png" alt="pic of fish" />
+    <br />
+    <button type="button" className="btn btn-primary">Register now</button>
+  </div>
+);
+
+export default MainContent;
